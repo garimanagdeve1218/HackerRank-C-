3b9298a2a94766e1c81005912712bf51646affe0
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-26 02:50:56 +0530

    Create 3. Basic Datatypes.cpp

---
+++ 3. Basic Datatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int n ;
+    long lg ;
+    char ch ; 
+    float f ; 
+    double d;
+    
+    cin>>n>>lg>>ch>>f>>d;
+    
+    // cin>>n;
+    // cin>>lg;
+    // cin>>ch ;
+    // cin>>f ;
+    // cin>>d ;
+    
+    // cout<< n << endl << lg << endl << ch << endl << f << endl <<d;
+    printf("%d\n%ld\n%c\n%f\n%lf\n", n, lg, ch, f, d);
+    return 0;
+}
