81eb96cf293b615318495efe83f450087c6d5371
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-27 12:48:33 +0530

    Update 6. Functions.cpp

--- 6. Functions.cpp
+++ 6. Functions.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
+    
+int max_of_four(int a, int b, int c, int d){
+    int max = a;
+    if(b>max){
+        max=b;
+    }
+    
+    if(c>max){
+        max=c;
+    }
+    if(d>max){
+        max=d;
+    }
+    return max;
+}
 
-
-
-int main() {
-    int a,b,c,d,max_of_four;
+int main(){
+    int a,b,c,d;
     cin>>a>>b>>c>>d;
-    if(a>b and a>c and a>d){
-        cout<<a;    
-    }
-    else if(b>a and b>c and b>d){
-        cout<<b;    
-    }
-    else if(c>a and c>b and c>d){
-        cout<<c;    
-    }
-    else{
-        cout<<d;    
-    }
-    
-    
+    int ans = max_of_four(a,b,c,d);
+    cout<<ans;
     
     return 0;
 }
+   
